5b77c5e513c6b74a5c67e169f0475994cd3d4c5a
Sandesh Bhattarai <sandesh1122bhattarai@gmail.com>
2025-06-11 17:59:35 -0500

    Final production change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : `${import.meta.env.VITE_API_URL}/api`; // Not just "/api"!
+
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api": import.meta.env.VITE_API_URL + "/api",
-    withCredentials: true,
-})
+  baseURL,
+  withCredentials: true,
+});
