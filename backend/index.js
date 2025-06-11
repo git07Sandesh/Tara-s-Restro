c531fcd44b752e136ab3333135a7b17f2ab3cc9a
Sandesh Bhattarai <sandesh1122bhattarai@gmail.com>
2025-06-11 17:29:45 -0500

    Modified some changes

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const __dirname = path.resolve();
 const app = express();
 // Add this after initializing express
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://tara-restro.onrender.com', "https://tara-s-restro-admin.onrender.com"],// Frontend URL
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://tara-s-restro-backend.onrender.com', "https://tara-s-restro-admin.onrender.com"],// Frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }));
